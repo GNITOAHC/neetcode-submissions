dd5ef78cf8da4ae5aae1379691b624473350ff41
Chao-Ting, Chen <chaotingchen10@gmail.com>
2026-06-02 16:57:43 +0800

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n == 1 || n == 2) return n;
+
+        vector<int> climb(n + 1, 0);
+
+        climb[1] = 1;
+        climb[2] = 2;
+        for (int i = 3; i < climb.size(); ++i) {
+            climb[i] = climb[i - 1] + climb[i - 2];
+        }
+
+        return climb[n];
+    }
+};
